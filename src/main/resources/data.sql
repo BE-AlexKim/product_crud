768082577203553e7422c69fbc7b0f82fcb27aeb
Developerbabe <118235272+BE-AlexKim@users.noreply.github.com>
2025-02-25 15:07:19 +0900

    관리자, 작가, 고객 데이터 각각 1개씩 생성하는 샘플 data.sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT tb_user_info(user_id,uuid,login_id, login_pw, phone_number, user_name, user_role, create_at)
+VALUES
+       (1,'test-user-001','rpp0321','$2a$10$HeaZl05.dfBfQag65Cf5wOiwaJubMjlr/Ld5JjFbi1qc1.PSdpD5e','01012345678','홍길동','ROLE_ADMIN',now()),
+       (2,'test-user-002','joy585','$2a$10$HeaZl05.dfBfQag65Cf5wOiwaJubMjlr/Ld5JjFbi1qc1.PSdpD5e','01098765432','임꺽정','ROLE_PARTNER',now()),
+       (3,'test-user-003','showx66','$2a$10$HeaZl05.dfBfQag65Cf5wOiwaJubMjlr/Ld5JjFbi1qc1.PSdpD5e','01021314151','성춘향','ROLE_USER',now());
