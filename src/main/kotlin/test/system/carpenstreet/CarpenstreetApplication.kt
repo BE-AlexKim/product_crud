e54bc7e23f0f78cea0bfd822f0816ff7a9667a6f
Developerbabe <118235272+BE-AlexKim@users.noreply.github.com>
2025-02-25 15:12:26 +0900

    @ConfigurationPropertiesScan 어노테이션 추가

--- src/main/kotlin/test/system/carpenstreet/CarpenstreetApplication.kt
+++ src/main/kotlin/test/system/carpenstreet/CarpenstreetApplication.kt
@@ -1,9 +1,12 @@
 package test.system.carpenstreet
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class CarpenstreetApplication
 
 fun main(args: Array<String>) {
